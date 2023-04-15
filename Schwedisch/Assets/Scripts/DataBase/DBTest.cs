2d0d1e2e7b47214d5a8421ce2a08e53c6c03503c
Urgush <orkurgush@gmail.com>
2023-04-15 22:42:46 +0200

    Daily Commit

---
+++ Schwedisch/Assets/Scripts/DataBase/DBTest.cs
@@ -0,0 +1,33 @@
+using System.Data;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+public class DBTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/Plugins/SQLite/Schwedisch.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT substantiv_id, singular_unbestimmt_schwedisch " + "FROM substantiv";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int value = reader.GetInt32(0);
+            string name = reader.GetString(1);
+
+            Debug.Log("value= " + value + "  name =" + name );
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+}
+
